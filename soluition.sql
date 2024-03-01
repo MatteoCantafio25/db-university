5e57dde1aab1a93fc23d3d40f2fbeb8bfe50200b
MatteoCantafio25 <mattyteo004@gmail.com>
2024-03-01 16:13:00 +0100

    Exercise 3 completed

--- soluition.sql
+++ soluition.sql
@@ -10,7 +10,9 @@ FROM `courses`
 WHERE `cfu` > 10;
 
 -- 3. Selezionare tutti gli studenti che hanno più di 30 anni
-
+SELECT `date_of_birth`
+FROM `students`
+WHERE YEAR(`date_of_birth`) < 1994;
 
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
 
