8f8dbd6fc5354779ad1d89e547426faca6d4c73b
MatteoCantafio25 <mattyteo004@gmail.com>
2024-03-01 17:22:23 +0100

    Exercise 1 (query with group) completed

--- soluition.sql
+++ soluition.sql
@@ -40,3 +40,22 @@ FROM `departments`;
 SELECT `name`, `surname`, `phone`
 FROM `teachers`
 WHERE `phone` IS NULL;
+
+
+-------------------------------------------------------------------------------
+
+-- QUERY WITH GROUP
+
+-- 1. Contare quanti iscritti ci sono stati ogni anno
+SELECT COUNT(`id`) AS `stud_every_year`, YEAR(`enrolment_date`) AS `enrolment_year`
+FROM `students`
+GROUP BY `enrolment_year`;
+
+-- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
+
+
+-- 3. Calcolare la media dei voti di ogni appello d'esame
+
+
+-- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
+
